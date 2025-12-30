f8747cf785846b1f3a316b65ed07d68c9f827f87
GebetaEst <estifanosgirma4@gmail.com>
2025-12-30 15:37:20 +0300

    2

---
+++ task_4.sql
@@ -0,0 +1,4 @@
+-- Display the full description (structure) of the 'Books' table
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'Books';
